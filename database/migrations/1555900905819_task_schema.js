74a54c234a78afb58b25fb20c364171631b447b2
Danilo Cordeiro <danilocordeiro.dev@gmail.com>
2019-04-21 23:53:08 -0300

    Add tasks and relationship with users

---
+++ database/migrations/1555900905819_task_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class TaskSchema extends Schema {
+  up () {
+    this.create('tasks', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('title').notNullable()
+      table.text('description').notNullable()
+      table.boolean('completed').defaultTo(false)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('tasks')
+  }
+}
+
+module.exports = TaskSchema
